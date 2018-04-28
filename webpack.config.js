5b302a124696769e71f8a558e91e37f7c09615ff
macbook pro yifan <yifan@magicmoma.com>
2018-04-28 14:16:04 +0800

    feat: 4-1 webpack 配置 css 单独分离打包

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HTMLPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
+const ExtractPlugin = require('extract-text-webpack-plugin')
 
 const isDev = process.env.NODE_ENV === 'development'
 
@@ -8,7 +9,7 @@ const config = {
     target: 'web',
     entry: path.join(__dirname, 'src/index.js'),
     output: {
-        filename: 'bundle.js',
+        filename: 'bundle.[hash:8].js',
         path: path.join(__dirname, 'dist')
     },
     module: {
@@ -20,27 +21,6 @@ const config = {
             {
                 test: /\.jsx/,
                 loader: 'babel-loader'
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.styl$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    {
-                        loader: "postcss-loader",
-                        options: {
-                            sourceMap: true,
-                        }
-                    },
-                    'stylus-loader' // 向上抛
-                ]
             },
             {
                 test: /\.(gif|jpg|jpeg|png|svg)$/,
@@ -64,9 +44,23 @@ const config = {
         }),
         new HTMLPlugin()
     ]
-};
+}
 
 if (isDev) {
+    config.module.rules.push({
+        test: /\.styl$/,
+        use: [
+            'style-loader',
+            'css-loader',
+            {
+                loader: 'postcss-loader',
+                options: {
+                    sourceMap: true,
+                }
+            },
+            'stylus-loader'
+        ]
+    })
     config.devtool = '#cheap-module-eval-source-map' // 调试代码
     config.devServer = {
         port: 8000,
@@ -80,6 +74,27 @@ if (isDev) {
     config.plugins.push(
         new webpack.HotModuleReplacementPlugin(),
         new webpack.NoEmitOnErrorsPlugin()
+    )
+} else {
+    config.output.filename = '[name].[chunkhash:8].js'
+    config.module.rules.push({
+        test: /\.styl$/,
+        use: ExtractPlugin.extract({
+            fallback: 'style-loader',
+            use: [
+                'css-loader',
+                {
+                    loader: 'postcss-loader',
+                    options: {
+                        sourceMap: true,
+                    }
+                },
+                'stylus-loader'
+            ]
+        })
+    }),
+    config.plugins.push(
+        new ExtractPlugin('styles.[contentHash:8].css')
     )
 }
 
